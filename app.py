572d59b93a589b41f68982919119216a63bac37b
Almamy Youssouf Ly <60490503+AYLY92@users.noreply.github.com>
2022-09-27 11:57:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@
 import streamlit as st
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import statsmodels
 
 import seaborn as sns # Visualization
 import matplotlib.pyplot as plt # Visualization
